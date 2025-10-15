3f0647b3c1b619d492008b96bd2e3f57caa2706c
Tony Dinh <88163609+tonydnh@users.noreply.github.com>
2025-10-14 19:05:16 -0500

    is even function (#23)

---
+++ src/iseven.py
@@ -0,0 +1,409 @@
+import random
+
+def is_even(num):
+    if num == 1:
+        return False
+    if num == 2:
+        return True
+    if num == 3:
+        return False
+    if num == 4:
+        return True
+    if num == 5:
+        return False
+    if num == 6:
+        return True
+    if num == 7:
+        return False
+    if num == 8:
+        return True
+    if num == 9:
+        return False
+    if num == 10:
+        return True
+    if num == 11:
+        return False
+    if num == 12:
+        return True
+    if num == 13:
+        return False
+    if num == 14:
+        return True
+    if num == 15:
+        return False
+    if num == 16:
+        return True
+    if num == 17:
+        return False
+    if num == 18:
+        return True
+    if num == 19:
+        return False
+    if num == 20:
+        return True
+    if num == 21:
+        return False
+    if num == 22:
+        return True
+    if num == 23:
+        return False
+    if num == 24:
+        return True
+    if num == 25:
+        return False
+    if num == 26:
+        return True
+    if num == 27:
+        return False
+    if num == 28:
+        return True
+    if num == 29:
+        return False
+    if num == 30:
+        return True
+    if num == 31:
+        return False
+    if num == 32:
+        return True
+    if num == 33:
+        return False
+    if num == 34:
+        return True
+    if num == 35:
+        return False
+    if num == 36:
+        return True
+    if num == 37:
+        return False
+    if num == 38:
+        return True
+    if num == 39:
+        return False
+    if num == 40:
+        return True
+    if num == 41:
+        return False
+    if num == 42:
+        return True
+    if num == 43:
+        return False
+    if num == 44:
+        return True
+    if num == 45:
+        return False
+    if num == 46:
+        return True
+    if num == 47:
+        return False
+    if num == 48:
+        return True
+    if num == 49:
+        return False
+    if num == 50:
+        return True
+    if num == 51:
+        return False
+    if num == 52:
+        return True
+    if num == 53:
+        return False
+    if num == 54:
+        return True
+    if num == 55:
+        return False
+    if num == 56:
+        return True
+    if num == 57:
+        return False
+    if num == 58:
+        return True
+    if num == 59:
+        return False
+    if num == 60:
+        return True
+    if num == 61:
+        return False
+    if num == 62:
+        return True
+    if num == 63:
+        return False
+    if num == 64:
+        return True
+    if num == 65:
+        return False
+    if num == 66:
+        return True
+    if num == 67:
+        return False
+    if num == 68:
+        return True
+    if num == 69:
+        return False
+    if num == 70:
+        return True
+    if num == 71:
+        return False
+    if num == 72:
+        return True
+    if num == 73:
+        return False
+    if num == 74:
+        return True
+    if num == 75:
+        return False
+    if num == 76:
+        return True
+    if num == 77:
+        return False
+    if num == 78:
+        return True
+    if num == 79:
+        return False
+    if num == 80:
+        return True
+    if num == 81:
+        return False
+    if num == 82:
+        return True
+    if num == 83:
+        return False
+    if num == 84:
+        return True
+    if num == 85:
+        return False
+    if num == 86:
+        return True
+    if num == 87:
+        return False
+    if num == 88:
+        return True
+    if num == 89:
+        return False
+    if num == 90:
+        return True
+    if num == 91:
+        return False
+    if num == 92:
+        return True
+    if num == 93:
+        return False
+    if num == 94:
+        return True
+    if num == 95:
+        return False
+    if num == 96:
+        return True
+    if num == 97:
+        return False
+    if num == 98:
+        return True
+    if num == 99:
+        return False
+    if num == 100:
+        return True
+    if num == 101:
+        return False
+    if num == 102:
+        return True
+    if num == 103:
+        return False
+    if num == 104:
+        return True
+    if num == 105:
+        return False
+    if num == 106:
+        return True
+    if num == 107:
+        return False
+    if num == 108:
+        return True
+    if num == 109:
+        return False
+    if num == 110:
+        return True
+    if num == 111:
+        return False
+    if num == 112:
+        return True
+    if num == 113:
+        return False
+    if num == 114:
+        return True
+    if num == 115:
+        return False
+    if num == 116:
+        return True
+    if num == 117:
+        return False
+    if num == 118:
+        return True
+    if num == 119:
+        return False
+    if num == 120:
+        return True
+    if num == 121:
+        return False
+    if num == 122:
+        return True
+    if num == 123:
+        return False
+    if num == 124:
+        return True
+    if num == 125:
+        return False
+    if num == 126:
+        return True
+    if num == 127:
+        return False
+    if num == 128:
+        return True
+    if num == 129:
+        return False
+    if num == 130:
+        return True
+    if num == 131:
+        return False
+    if num == 132:
+        return True
+    if num == 133:
+        return False
+    if num == 134:
+        return True
+    if num == 135:
+        return False
+    if num == 136:
+        return True
+    if num == 137:
+        return False
+    if num == 138:
+        return True
+    if num == 139:
+        return False
+    if num == 140:
+        return True
+    if num == 141:
+        return False
+    if num == 142:
+        return True
+    if num == 143:
+        return False
+    if num == 144:
+        return True
+    if num == 145:
+        return False
+    if num == 146:
+        return True
+    if num == 147:
+        return False
+    if num == 148:
+        return True
+    if num == 149:
+        return False
+    if num == 150:
+        return True
+    if num == 151:
+        return False
+    if num == 152:
+        return True
+    if num == 153:
+        return False
+    if num == 154:
+        return True
+    if num == 155:
+        return False
+    if num == 156:
+        return True
+    if num == 157:
+        return False
+    if num == 158:
+        return True
+    if num == 159:
+        return False
+    if num == 160:
+        return True
+    if num == 161:
+        return False
+    if num == 162:
+        return True
+    if num == 163:
+        return False
+    if num == 164:
+        return True
+    if num == 165:
+        return False
+    if num == 166:
+        return True
+    if num == 167:
+        return False
+    if num == 168:
+        return True
+    if num == 169:
+        return False
+    if num == 170:
+        return True
+    if num == 171:
+        return False
+    if num == 172:
+        return True
+    if num == 173:
+        return False
+    if num == 174:
+        return True
+    if num == 175:
+        return False
+    if num == 176:
+        return True
+    if num == 177:
+        return False
+    if num == 178:
+        return True
+    if num == 179:
+        return False
+    if num == 180:
+        return True
+    if num == 181:
+        return False
+    if num == 182:
+        return True
+    if num == 183:
+        return False
+    if num == 184:
+        return True
+    if num == 185:
+        return False
+    if num == 186:
+        return True
+    if num == 187:
+        return False
+    if num == 188:
+        return True
+    if num == 189:
+        return False
+    if num == 190:
+        return True
+    if num == 191:
+        return False
+    if num == 192:
+        return True
+    if num == 193:
+        return False
+    if num == 194:
+        return True
+    if num == 195:
+        return False
+    if num == 196:
+        return True
+    if num == 197:
+        return False
+    if num == 198:
+        return True
+    if num == 199:
+        return False
+    if num == 200:
+        return True
+    return num % 2 == 0
+
+if __name__ == "__main__":
+    num = random.randint(1, 200)
+    even = is_even(num)
+    print(f"Is {num} even? {'Yes!' if even else 'No!'}")
